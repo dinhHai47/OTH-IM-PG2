9f8aedb0bc6bb4d94c0fa63b8e10a977f7df0e6b
SuperMario4848 <34087110+SuperMario4848@users.noreply.github.com>
2020-06-06 12:05:59 +0200

    renamed file

--- Kapitel_01/src/guide/Hello.java
+++ Kapitel_01/src/guide/Hello.java
@@ -1,7 +1,7 @@
 package guide;
 
 //Try to compile this file in the terminal and execute the result.
-public class Main {
+public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
