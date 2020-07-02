5e99d41811cec7820388e3ddedfea527bdb2e529
SuperMario4848 <34087110+SuperMario4848@users.noreply.github.com>
2020-07-02 18:40:18 +0200

    renamed Class

--- Kapitel_06/src/uebung_6_3/Lettermixer.java
+++ Kapitel_06/src/uebung_6_3/Lettermixer.java
@@ -2,7 +2,7 @@ package uebung_6_3;
 
 import java.util.Scanner;
 
-public class Buchstabenmixer {
+public class Lettermixer {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
